643637261e9b16deaedc242f30a627db30081255
Good5263 <np50022005@yandex.ru>
2021-04-04 03:05:07 +0400

    update animation

--- app.py
+++ app.py
@@ -8,50 +8,47 @@ from flask import Flask, render_template, abort, request, jsonify, redirect
 import database
 
 
-def create_app():
-    app = Flask(__name__)
-    run_with_ngrok(app)
+app = Flask(__name__)
+run_with_ngrok(app)
 
-    @app.route("/")
-    def hello_world():
-        return render_template("index.html")
+@app.route("/")
+def hello_world():
+    return render_template("index.html")
 
-    @app.route("/<url>")
-    def redirect_to_url(url):
-        if not database.new_url_exists(url):
-            return abort(404)
-        else:
-            database.quantity_increment(url)
-            return redirect(database.get_link(url))
+@app.route("/<url>")
+def redirect_to_url(url):
+    if not database.new_url_exists(url):
+        return abort(404)
+    else:
+        database.quantity_increment(url)
+        return redirect(database.get_link(url))
 
-    @app.route("/new", methods=['POST'])
-    def get_link():
-        json_ = request.get_json(force=True)
-        response = {
-            "success": True,
-            "error": None,
-            "link": "ok"
-        }
+@app.route("/new", methods=["POST"])
+def get_link():
+    json_ = request.get_json(force=True)
+    response = {
+        "success": True,
+        "error": None,
+        "link": "ok"
+    }
 
-        pattern = re.compile(r"((ftp|http|https):\/\/)?(www\.)?([A-Za-zА-Яа-я0-9]{1}[A-Za-zА-Яа-я0-9\-]*\.?)*\.{1}[A-Za-zА-Яа-я0-9-]{2,8}(\/([\w#!:.?+=&%@!\-\/])*)?")
-        if not re.match(pattern, json_["link"]):
-            response['success'] = False
-            response["error"] = "Provided link is not a URL"
-        else:
-            new_url = database.receive_new_url(json_["link"])
-            response["link"] = request.url_root + new_url
+    pattern = re.compile(r"((ftp|http|https):\/\/)?(www\.)?([A-Za-zА-Яа-я0-9]{1}[A-Za-zА-Яа-я0-9\-]*\.?)*\.{1}[A-Za-zА-Яа-я0-9-]{2,8}(\/([\w#!:.?+=&%@!\-\/])*)?")
+    if not re.match(pattern, json_["link"]):
+        response['success'] = False
+        response["error"] = "Provided link is not a URL"
+    else:
+        new_url = database.receive_new_url(json_["link"])
+        response["link"] = request.url_root + new_url
 
-        return jsonify(response)
+    return jsonify(response)
     
-    @app.route("/all_links")
-    def show_all_links():
-        return render_template("links.html", links=database.get_all_dicts())
+@app.route("/all_links")
+def show_all_links():
+    return render_template("links.html", links=database.get_all_dicts())
 
-    @app.errorhandler(404)
-    def page_not_found(e):
-        return render_template("page_not_found.html"), 404
-
-    return app
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template("page_not_found.html"), 404
 
 
 def start_creating_db_backups():
@@ -66,5 +63,4 @@ def start_creating_db_backups():
 
 if __name__ == "__main__":
     start_creating_db_backups()
-    app = create_app()
     app.run()
--- static/js/index.js
+++ static/js/index.js
@@ -33,11 +33,3 @@ function swapTextAndIcon() {
         document.getElementById("copy_button").insertAdjacentHTML("afterbegin", icon)
     }, 1500)
 }
-
-const shorten_button = document.getElementById("short_url_button")
-shorten_button.addEventListener("mouseover", () => shorten_button.classList.add("white_icon"), true)
-shorten_button.addEventListener("mouseout", () => shorten_button.classList.remove('white_icon'), true)
-
-const copy_button = document.getElementById("copy_button")
-copy_button.addEventListener("mouseover", () => copy_button.classList.add("white_icon"), true)
-copy_button.addEventListener("mouseout", () => copy_button.classList.remove("white_icon"), true)
