ccb94fd30f9f16a5adef705e774b9d9600eaa9e2
Good5263 <np50022005@yandex.ru>
2021-06-09 13:53:13 +0400

    renaming

--- static/js/index.js
+++ static/js/index.js
@@ -2,19 +2,19 @@ async function receiveShortLink() {
     let response = await fetch(window.location.href + "/new", {
         method: "POST",
         headers: { "Content-Type": "application/json;charset=utf-8" },
-        body: JSON.stringify({ "link": document.getElementById("user_link").value })
+        body: JSON.stringify({ "link": document.getElementById("user-link").value })
     });
 
     let result = await response.json();
     if (result.success) {
-        document.getElementById("new_link").value = result.link
+        document.getElementById("new-generated-link").value = result.link
     } else {
-        document.getElementById("new_link").value = result.error
+        document.getElementById("new-generated-link").value = result.error
     }
 }
 
 function copyText() {
-    let text = document.getElementById("new_link");
+    let text = document.getElementById("new-generated-link");
     text.select();
     document.execCommand("copy");
     text.value += ' ';
@@ -23,13 +23,13 @@ function copyText() {
 }
 
 function swapTextAndIcon() {
-    const icon = document.getElementById("copy_button").innerHTML
-    document.getElementById("copy_button_icon").remove()
-    document.getElementById("copy_button").insertAdjacentHTML(
-        "afterbegin", '<span class="copy_button_text" id="copy_button_text">Copied</span>'
+    const icon = document.getElementById("button_copy-text").innerHTML
+    document.getElementById("button_copy-text__icon").remove()
+    document.getElementById("button_copy-text").insertAdjacentHTML(
+        "afterbegin", '<span class="button_copy-text_clicked" id="button_copy-text_clicked">Copied</span>'
     )
     setTimeout(() => {
-        document.getElementById("copy_button_text").remove()
-        document.getElementById("copy_button").insertAdjacentHTML("afterbegin", icon)
+        document.getElementById("button_copy-text_clicked").remove()
+        document.getElementById("button_copy-text").insertAdjacentHTML("afterbegin", icon)
     }, 1500)
 }
