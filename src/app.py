50a91c5c477cdc0a3bb3c368a696c62d7ddf50b6
Good5263 <np50022005@yandex.ru>
2021-06-11 05:06:42 +0400

    added logging

--- src/app.py
+++ src/app.py
@@ -1,12 +1,18 @@
 import re
+import logging
 from math import ceil
 
 import waitress
 from flask import Flask, render_template, abort, request, jsonify, redirect
 from werkzeug.exceptions import HTTPException
 
+from logger import Logger
 from database import Database
 
+
+logger = Logger()
+logger.add_logger("debug", "logs/debug.log", logging.WARNING, "[%(asctime)s] %(levelname)s\n%(message)s")
+logger.add_logger("requests", "logs/requests.log", logging.INFO)
 
 app = Flask(__name__)
 db = Database()
@@ -21,6 +27,8 @@ def index():
 def redirect_to_url(url):
     if not db.short_url_exists(url):
         return abort(404)
+    
+    logger.requests.info(f"[302] [Successfully redirected from /{url}]")
 
     db.quantity_increment(url)
     return redirect(db.get_link(url))
@@ -51,7 +59,8 @@ def get_new_link():
 
     if response["success"]:
         short_url = db.receive_short_url(request.json["link"])
-        response["link"] = request.url_root + short_url 
+        response["link"] = request.url_root + short_url
+        logger.requests.info(f"[200] [Received short url ({short_url}) for link ({request.json['link']})]")
     else:
         response["error"] = "Provided link is not a URL"
 
@@ -67,7 +76,10 @@ def handle_error(error):
 
     code = 500
     if isinstance(error, HTTPException):
+        logger.requests.info(f"[{error.code}] [{error.description}]")
         code = error.code
+    else:
+        logger.debug.exception(error)
     
     code_value = status_codes.get(code, "Unknown error")
 
@@ -75,4 +87,5 @@ def handle_error(error):
 
 
 if __name__ == "__main__":
-    waitress.serve(app)
+    #waitress.serve(app)
+    app.run()
