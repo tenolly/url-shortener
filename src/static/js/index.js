15ba0e991c26777cce17ad7d8929b5aef665f79d
Good5263 <np50022005@yandex.ru>
2021-06-12 18:16:06 +0400

    added error handler

--- src/static/js/index.js
+++ src/static/js/index.js
@@ -1,15 +1,17 @@
 async function receiveShortLink() {
-    let response = await fetch(window.location.href + "/new", {
+    const response = await fetch(window.location.href + "/new_link", {
         method: "POST",
         headers: { "Content-Type": "application/json;charset=utf-8" },
         body: JSON.stringify({ "link": document.getElementById("user-link").value })
     });
 
-    let result = await response.json();
-    if (result.success) {
-        document.getElementById("new-generated-link").value = result.link
+    const generated_link =  document.getElementById("new-generated-link");
+
+    if (response.ok) {
+        const result = await response.json();
+        generated_link.value = result.success ? result.link : result.error;
     } else {
-        document.getElementById("new-generated-link").value = result.error
+        generated_link.value = `Error ${response.status}`;
     }
 }
 
@@ -17,19 +19,19 @@ function copyText() {
     let text = document.getElementById("new-generated-link");
     text.select();
     document.execCommand("copy");
-    text.value += ' ';
+    text.value += " ";
     text.value = text.value.slice(0, -1);
-    swapTextAndIcon()
+    swapTextAndIcon();
 }
 
 function swapTextAndIcon() {
-    const icon = document.getElementById("button_copy-text").innerHTML
-    document.getElementById("button_copy-text__icon").remove()
+    const icon = document.getElementById("button_copy-text").innerHTML;
+    document.getElementById("button_copy-text__icon").remove();
     document.getElementById("button_copy-text").insertAdjacentHTML(
         "afterbegin", '<span class="button_copy-text_clicked" id="button_copy-text_clicked">Copied</span>'
-    )
+    );
     setTimeout(() => {
         document.getElementById("button_copy-text_clicked").remove()
         document.getElementById("button_copy-text").insertAdjacentHTML("afterbegin", icon)
-    }, 1500)
+    }, 1500);
 }
